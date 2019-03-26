8026beaf7a2691c7060619c97041b5d31a5d9b4e
Ryan Slama <rockinryguy@gmail.com>
2019-03-25 20:56:44 -0400

    Bluescreen typo

--- src/Bluescreen.tsx
+++ src/Bluescreen.tsx
@@ -6,7 +6,7 @@ const Home = React.memo(
         return <div id="bluescreen">
             <p>A problem has been detected and Windows has been shut down to prevent damage to your computer.</p>
             <p>If this is the first time you've seen this stop error screen, restart your computer. if this screen appears again, follow these steps:</p>
-            <p>Check to make sure any new hardware or software is properly installed. I this is a new installation, ask your hardware or software manufacturer for and Windows updates you might need.</p>
+            <p>Check to make sure any new hardware or software is properly installed. If this is a new installation, ask your hardware or software manufacturer for and Windows updates you might need.</p>
             <p>If problems continue, disable or remove any newly installed hardware or software. Disable BIOS memory options such as caching or shadowing. If you need to use Safe Mode to remove or disable components, restart your computer, press F8 to select Advanced Startup Options, and then select Safe Mode.</p>
             <p>Technical information:</p>
             <p>*** STOP: 0x000000FE (0x00000008, 0x000000006, 0x00000009, 0x847075cc)</p>
